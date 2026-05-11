b26121901bfefff74e725275dd90d959bb9ba48a
Soham-Kakkar <kakkarsoham@gmail.com>
2026-05-11 13:48:41 +0530

    server build test and signin page fix

--- client/src/app/auth/github/page.tsx
+++ client/src/app/auth/github/page.tsx
@@ -11,12 +11,14 @@ export default function GithubAuthPage() {
   }
 
   return (
-      <Card className="max-w-md mx-auto text-center">
+    <div className="flex justify-center items-center">
+      <Card className="max-w-md mx-auto text-center h-fit">
         <CardHeader className="text-xl font-semibold mb-4">Sign in with GitHub</CardHeader>
         <CardContent className="flex flex-col gap-4">
           <p className="text-sm mb-6">Securely authenticate using your GitHub account.</p>
           <Button onClick={startGithub}>Continue with GitHub</Button>
         </CardContent>
       </Card>
+    </div>
   );
 }
